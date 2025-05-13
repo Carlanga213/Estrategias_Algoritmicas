9652211511fb1fe65c2702683a56aaa6e8ca061d
vela020 <145611645+vela020@users.noreply.github.com>
2025-05-12 21:35:04 -0600

    Create E08_funciones_arrays_lists.py

---
+++ Notas_Libreria/codigos_ejercicios/E08_funciones_arrays_lists.py
@@ -0,0 +1,19 @@
+import heapq
+from collections import Counter
+
+def pqSort(strArray: list[str]) -> None:
+    heapq.heapify(strArray)
+    for i in range(len(strArray)):
+        strArray[i] = heapq.heappop(strArray)
+
+def hasDuplicates(strArray: list[str]) -> bool:
+    seen = set()
+    for word in strArray:
+        if word in seen:
+            return True
+        seen.add(word)
+    return False
+
+def mode(intArray: list[int]) -> int:
+    count = Counter(intArray)
+    return count.most_common(1)[0][0]
